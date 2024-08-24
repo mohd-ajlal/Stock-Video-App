7491c9cd13bad3d22947ed27a0728cdc41a04a7c
Mohd Ajlal <ajlal00786@gmail.com>
2024-08-24 23:35:30 +0530

    sign in

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -39,7 +39,13 @@ const RootLayout = () => {
   return (
     <Stack>
         <Stack.Screen name = "index" options={{headerShown:false}}/>
+        <Stack.Screen name = "(auth)" options={{headerShown:false}}/>
+        <Stack.Screen name = "(tabs)" options={{headerShown:false}}/>
+        {/* <Stack.Screen name = "/search/[query]" options={{headerShown:false}}/> */}
+
+
     </Stack>
+
 
 )
 }
